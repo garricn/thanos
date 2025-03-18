3f2463a02532c2955762018236d2e71096d9f982
Garric Nahapetian <garricn@icloud.com>
2025-03-17 21:27:30 -0700

    Create sonar report

--- jestSonar.config.js
+++ jestSonar.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  reportPath: './coverage',
-  reportFile: 'sonar-report.xml',
+  reportPath: process.env.JEST_SONAR_REPORTER_OUTPUT_DIR || './coverage',
+  reportFile: process.env.JEST_SONAR_REPORTER_OUTPUT_NAME || 'sonar-report.xml',
   indent: 2,
   sonarqubeVersion: '9.9',
   relativePaths: true,
