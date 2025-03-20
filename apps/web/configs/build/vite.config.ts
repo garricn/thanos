95cada2687d2da40dc8c6dab37d59b2c8a9d183b
Garric Nahapetian <garricn@icloud.com>
2025-03-19 17:23:06 -0700

    refactor: move PostCSS and Tailwind configs to configs/build

--- apps/web/configs/build/vite.config.ts
+++ apps/web/configs/build/vite.config.ts
@@ -1,34 +1,37 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(() => ({
-  root: path.resolve(__dirname, '../../../../apps/web'),
-  cacheDir: '../../../../node_modules/.vite/web',
+  root: path.resolve(__dirname, "../../../../apps/web"),
+  cacheDir: "../../../../node_modules/.vite/web",
   server: {
     port: 4200,
-    host: 'localhost',
+    host: "localhost",
     proxy: {
-      '/api': 'http://localhost:3000',
+      "/api": "http://localhost:3000",
     },
   },
   preview: {
     port: 4300,
-    host: 'localhost',
+    host: "localhost",
   },
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
+  },
+  css: {
+    postcss: "../../../../configs/build/postcss.config.js",
   },
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [],
   // },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
