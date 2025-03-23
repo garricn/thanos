fd04d5b6a2c5b06cdcd300aa0b0170bef186d9dc
Garric Nahapetian <garricn@icloud.com>
2025-03-23 15:34:06 -0700

    feat: setup playwright e2e tests

---
+++ apps/web/playwright.config.ts
@@ -0,0 +1,24 @@
+import test from '@playwright/test';
+
+const config = {
+  testDir: './e2e',
+  fullyParallel: true,
+  reporter: 'html',
+  use: {
+    baseURL: 'http://localhost:4200',
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...test.devices['Desktop Chrome'] },
+    },
+  ],
+};
+
+export default config;
