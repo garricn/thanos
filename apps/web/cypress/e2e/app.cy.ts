a92be4b4b89c7698b8e2f8d12c4638278e489f4f
Garric Nahapetian <garricn@icloud.com>
2025-03-23 15:37:11 -0700

    chore: remove cypress and update dependencies

--- apps/web/cypress/e2e/app.cy.ts
+++
@@ -1,9 +0,0 @@
-describe('App', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
-
-  it('should display hello world heading', () => {
-    cy.get('h1').should('contain', 'Hello World');
-  });
-});
