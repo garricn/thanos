0b26784a0385d510095284e0fee24932a8bcc261
Garric Nahapetian <garricn@icloud.com>
2025-03-23 22:00:45 -0700

    refactor: simplify git hooks to just run npm run check

--- scripts/hooks/pre-commit.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { runPreCommitChecks } from './git-hooks.js';
-
-// Execute pre-commit checks and exit with appropriate code
-runPreCommitChecks()
-  .then(() => process.exit(0))
-  .catch(() => process.exit(1));
